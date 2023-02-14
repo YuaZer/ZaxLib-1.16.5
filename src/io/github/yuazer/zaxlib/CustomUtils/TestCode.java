e7c8befe9cfc5de0a49dbb65cf51e47800ddc88d
YuaZer <1109132@qq.com>
2023-02-14 21:53:27 +0800

    增加 云端Yaml读取工具

--- src/io/github/yuazer/zaxlib/CustomUtils/TestCode.java
+++ src/io/github/yuazer/zaxlib/CustomUtils/TestCode.java
@@ -1,59 +1,30 @@
 package io.github.yuazer.zaxlib.CustomUtils;
 
 
-import io.github.yuazer.zaxlib.Utils.YamlUtils;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class TestCode {
     public static void main(String[] args) {
-        initWindow();
+        List<String> list = new ArrayList<>();
+        list.add("&aaa");
+        list.add("&bbb");
+        list.add("&ccc");
+        for (int i=0;i<list.size();i++){
+            System.out.println(list.get(i).replace("&","t"));
+        }
     }
-    public static void initWindow(){
-        JFrame jFrame = new JFrame("GraceZ Mod[1.16.5]");
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jFrame.setSize(400,500);
-        jFrame.setVisible(true);
-    }
-//    public static Object getKey(HashMap map, Object value) {
-//        List<Object> keyList = new ArrayList<>();
-//        for (Object key : map.keySet()) {
-//            if (map.get(key).equals(value)) {
-//                keyList.add(key);
-//            }
-//        }
-//        return keyList;
-//    }
-//    public static void readTxtFile(String filePath) {
-//        try {
-//            List<String> keyList = new ArrayList<>();
-//            String encoding = "GBK";
-//            File file = new File(filePath);
-//            if (file.isFile() && file.exists()) { //判断文件是否存在
-//                InputStreamReader read = new InputStreamReader(new FileInputStream(file), encoding);//考虑到编码格式
-//                BufferedReader bufferedReader = new BufferedReader(read);
-//                String lineTxt = null;
-//                while ((lineTxt = bufferedReader.readLine()) != null) {
-//                    keyList.add(lineTxt);
-//                    System.out.println(keyList);
-//                }
-//                read.close();
-//            } else {
-//                System.out.println("找不到指定的文件");
-//            }
-//        } catch (Exception e) {
-//            System.out.println("读取文件内容出错");
-//            e.printStackTrace();
-//        }
-//    }
 }
 
--- src/io/github/yuazer/zaxlib/Utils/NetClassLoader.java
+++ src/io/github/yuazer/zaxlib/Utils/NetClassLoader.java
@@ -2,14 +2,16 @@ package io.github.yuazer.zaxlib.Utils;
 
 import io.github.yuazer.cltp.Main;
 import io.github.yuazer.cltp.TestInterface;
+import org.bukkit.configuration.file.YamlConfiguration;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
 
 public class NetClassLoader {
     public static Class getUrlClass(String jarUrl, String classFullName) {
@@ -24,14 +26,43 @@ public class NetClassLoader {
         }
         return clazz1;
     }
-
-    public static void main(String[] args) throws Exception {
-        URL[] urls = new URL[]{new URL("https://yuazer.github.io/myH5/ClassLoaderTestPlugin.jar")};
-        URLClassLoader urlClassLoader = new URLClassLoader(urls);
-        Class clazz1 = urlClassLoader.loadClass("io.github.yuazer.cltp.Main");
-        clazz1.getDeclaredConstructor().setAccessible(true);
-        Object obj1 = clazz1.newInstance();
-        Method m = clazz1.getDeclaredMethod("sout");
-        m.invoke(obj1);
+    public static YamlConfiguration getYamlFromUrl(String Url) {
+        //从URL获取YAML文件
+        InputStreamReader reader = null;
+        try {
+            URL url = new URL(Url);
+            InputStream in = url.openStream();// 从 URL 中获取文件byte流
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();//byte输出流
+            byte[] buffer = new byte[4096]; //定义4096长度的byte数组
+            int n;
+            while ((n = in.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, n); //把文件的字节输出流写进buffer
+            }
+            byte[] fileBytes = outputStream.toByteArray(); //将byte集合输出流转为byte数组
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(fileBytes); //将byte数组写进byte数组输入流
+            reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+            //从Reader读取YAML文件
+            return YamlConfiguration.loadConfiguration(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return YamlConfiguration.loadConfiguration(reader);
+    }
+    public static void main(String[] args) {
+        try {
+//            URL[] urls = new URL[]{new URL("https://yuazer.github.io/myH5/ClassLoaderTestPlugin.jar")};
+//            URLClassLoader urlClassLoader = new URLClassLoader(urls);
+//            Class clazz1 = urlClassLoader.loadClass("io.github.yuazer.cltp.Main");
+            Class clazz1 = getUrlClass("https://yuazer.github.io/myH5/ClassLoaderTestPlugin.jar","io.github.yuazer.cltp.Main");
+            Constructor c = clazz1.getDeclaredConstructor();
+            c.setAccessible(true);
+            Object obj1 = clazz1.newInstance();
+            Method m = clazz1.getDeclaredMethod("test");
+            System.out.println(m.invoke(obj1));
+        }catch (InstantiationException|IllegalAccessException| InvocationTargetException e){
+            e.printStackTrace();
+        }catch (NoSuchMethodException e){
+            System.out.println("类或方法不存在");
+        }
     }
 }
